fdb5ed229767209af81f7e06bc4ce99b7bcc1fe6
Tamersa36 <tamer8@live.com>
2024-07-14 00:59:55 +0300

    fix date

--- src/app/pages/apps/contacts/components/contacts-edit/contacts-edit.component.ts
+++ src/app/pages/apps/contacts/components/contacts-edit/contacts-edit.component.ts
@@ -141,6 +141,6 @@ export class ContactsEditComponent implements OnInit {
     const year = date.getFullYear();
     const month = ('0' + (date.getMonth() + 1)).slice(-2);
     const day = ('0' + date.getDate()).slice(-2);
-    return `${year}-${month}-${day}T00:00:00`;
+    return `${year}-${month}-${day}`;
   }
 }
